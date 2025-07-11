0842578938c43dcad150671c05701f3503ac654e
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-07-11 22:40:51 +0530

    style: styling the table according to the figma

--- src/components/TimesheetTable.tsx
+++ src/components/TimesheetTable.tsx
@@ -15,11 +15,11 @@ interface Props {
 const getStatusStyle = (status: string) => {
   switch (status) {
     case "COMPLETED":
-      return "bg-green-100 text-green-700";
+      return "bg-green-100 text-green-800";
     case "INCOMPLETE":
-      return "bg-yellow-100 text-yellow-700";
+      return "bg-yellow-100 text-yellow-800";
     case "MISSING":
-      return "bg-red-100 text-red-700";
+      return "bg-pink-100 text-pink-800";
     default:
       return "bg-gray-100 text-gray-600";
   }
@@ -38,29 +38,32 @@ const getActionText = (status: string) => {
 
 export default function TimesheetTable({ timesheets }: Props) {
   return (
-    <table className="w-full table-auto border rounded">
+    
+    <div className="bg-white  p-6 rounded-lg shadow-md">
+  <h3 className="text-2xl font-bold mb-4">Your Timesheets</h3>
+    <table className="w-full table-auto border border-gray-200 ">
       <thead>
-        <tr className="bg-gray-100 text-left">
-          <th className="p-2">Week #</th>
-          <th className="p-2">Date</th>
-          <th className="p-2">Status</th>
-          <th className="p-2">Actions</th>
+        <tr className="bg-gray-50 text-left p-4 text-gray-500">
+          <th className="p-4 w-[107px]">Week #</th>
+          <th className="p-4 w-[502px]">Date</th>
+          <th className="p-4 w-[502px]">Status</th>
+          <th className="p-4 w-[121px]">Actions</th>
         </tr>
       </thead>
       <tbody>
         {timesheets.map((ts) => (
-          <tr key={ts.id} className="border-t">
-            <td className="p-2">{ts.week}</td>
-            <td className="p-2">{ts.date}</td>
-            <td className="p-2">
-              <span className={`px-2 py-1 text-sm rounded-full ${getStatusStyle(ts.status)}`}>
+          <tr key={ts.id} className="border-t border-gray-300 hover:bg-gray-50">
+            <td className="p-4  border-gray-300 bg-gray-50 text-gray-900">{ts.week}</td>
+            <td className="p-4 text-gray-500">{ts.date}</td>
+            <td className="p-4">
+              <span className={`px-2.5 py-1 text-sm rounded-md ${getStatusStyle(ts.status)}`}>
                 {ts.status}
               </span>
             </td>
-            <td className="p-2">
+            <td className="p-4">
               <Link
                 href={`/timesheet/${ts.week}`}
-                className="text-blue-600 underline"
+                className="text-blue-600 "
               >
                 {getActionText(ts.status)}
               </Link>
@@ -69,5 +72,6 @@ export default function TimesheetTable({ timesheets }: Props) {
         ))}
       </tbody>
     </table>
+    </div>
   );
 }
