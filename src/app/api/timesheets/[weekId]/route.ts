f1546e7e9eb24194710192269e43eac4c938bd72
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-07-11 19:14:31 +0530

    feat(api): implement  dynamic route /api/timesheets/[weekId] to return entries for selected week

---
+++ src/app/api/timesheets/[weekId]/route.ts
@@ -0,0 +1,15 @@
+// app/api/timesheets/[weekId]/route.ts
+import { timesheetEntries } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { weekId: string } }
+) {
+  const weekData = timesheetEntries[params.weekId];
+  if (!weekData) {
+    return new NextResponse("Timesheet not found", { status: 404 });
+  }
+
+  return NextResponse.json(weekData); // contains `range` and `entries`
+}
