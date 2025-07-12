27a7004baabea8b974988935ac5231658727c311
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-07-12 07:07:18 +0530

    style(Footer): made the footer pade responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-8 p-8 rounded-lg bg-white text-center text-sm text-gray-500 shadow-sm ">
+    <footer className="mt-8 px-4 sm:px-6 md:px-8 py-6 rounded-lg bg-white text-center text-sm text-gray-500 shadow-sm max-w-screen-xl mx-auto ">
       &copy; 2024 tentwenty. All rights reserved.
     </footer>
   );
